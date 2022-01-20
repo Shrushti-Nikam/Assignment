e2dbc044e1c2a9afcbb29fea32526b1683db0339
Shrushti-Nikam <90201208+Shrushti-Nikam@users.noreply.github.com>
2022-01-20 12:17:12 +0530

    Add files via upload

---
+++ assignment 2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+marks = input("Enter your marks: ")
+print(f'My name is {name} and my age is {age} and i got {marks} marks ')
+items = [name, age, marks]
+print(items)
+
