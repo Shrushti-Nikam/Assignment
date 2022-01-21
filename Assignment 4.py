dc0701f9cbd15b18d2367173c7d4c70162f2cfc4
Shrushti-Nikam <90201208+Shrushti-Nikam@users.noreply.github.com>
2022-01-21 20:56:47 +0530

    Add files via upload

---
+++ Assignment 4.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+
+num = 24
+name = "Harry"
+num1 = 65.67
+x = True
+y = {"orange", "banana", "cherry"}
+z = [1, 2, 3, 4]
+w = ("good", "nice", "amazing")
+print("Integer:", num)
+print("String:", name)
+print("float:", num1)
+print("boolean:", x)
+print("set:", y)
+print("list:", z)
+print("tuple:", w)
+
+
+# In[9]:
+
+
+#list
+numbers = [1, 6, 9, 10, 17]
+print(f'the numbers in the list are  {*numbers,}')
+
+
+# In[7]:
+
+
+#list
+num1 = 5
+num2 = 10
+list1 = [num1, num2]
+print(list1)
+
